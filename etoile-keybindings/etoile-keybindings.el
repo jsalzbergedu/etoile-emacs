b05a04d0df59b57f4a7eca9018d9e398a1309fb9
Jacob Salzberg <jssalzbe@ncsu.edu>
2019-01-29 20:46:53 -0500

    Start working on moving keybindings over, add readme

---
+++ etoile-keybindings/etoile-keybindings.el
@@ -0,0 +1,32 @@
+;;; etoile-keybindings.el --- Keybindings for etoile
+
+;; Copyright © 2019 Jacob Salzberg
+
+;; Author: Jacob Salzberg <jssalzbe@ncsu.edu>
+;; URL: https://github.com/jsalzbergedu/etoile-keybindings
+;; Version: 0.1.0
+;; Keywords: keybindings etoile
+
+;; This file is not a part of GNU Emacs
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+(message "bonjour")
+
+
+(provide 'etoile-keybindings)
+;;; etoile-keybindings.el ends here
