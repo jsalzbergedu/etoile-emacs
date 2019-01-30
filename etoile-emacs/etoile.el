fc573a7fe7aadbc9d4363b9f5c7788975c718668
Jacob Salzberg <jssalzbe@ncsu.edu>
2019-01-29 19:25:14 -0500

    Begin creating new config

---
+++ etoile-emacs/etoile.el
@@ -0,0 +1,37 @@
+;;; etoile.el --- A modular emacs config using the straight package manager -*- lexical-binding: t -*-
+
+;; Copyright © 2019 Jacob Salzberg
+
+;; Author: Jacob Salzberg <jssalzbe@ncsu.edu>
+;; URL: https://github.com/jsalzbergedu/etoile-emacs
+;; Version: 0.1.0
+;; Keywords: etoile emacs config
+
+;; This file is not a part of GNU Emacs
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; The entry point for the etoile config
+
+;;; Code:
+(use-package etiole-themes
+  :straight (etoile-themes :type git
+                           :host github
+                           :repo "jsalzbergedu/etoile-emacs"
+                           :files ("etiole-themes/etiole-themes.el"))
+  :demand t)
+
+(provide 'etoile)
+;;; etoile.el ends here
---
+++ init.el
@@ -0,0 +1,22 @@
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(push "lib" straight-default-files-directive)
+(straight-use-package 'use-package)
+
+(use-package etiole
+  :straight (etiole :type git
+                    :host github
+                    :repo "jsalzbergedu/etoile-emacs"
+                    :files ("etoile-emacs/etiole.el"))
+  :demand t)
